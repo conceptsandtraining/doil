9ae9c34811c6dff2b63818382f427aa061be7244
lauraquellmalz <quellmalz.laura@gmail.com>
2020-03-09 15:16:35 +0100

    Finished!

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ cp -r src/lib/* /usr/lib/doil
 chmod -R 777 /usr/lib/doil/tpl/repos/
 
 # INSTALL MANPAGE
-#install -g 0 -o 0 -m 0644 src/man/doil.1 /usr/share/man/man1/
-#gzip /usr/share/man/man1/doil.1
+install -g 0 -o 0 -m 0644 src/man/doil.1 /usr/share/man/man1/
+gzip /usr/share/man/man1/doil.1
 
 # Install local instance tracker
-#read -p "Please type your username for this machine: " username
-#if [ ! -d "/home/$username/.doil" ]
-#then
-#  mkdir "/home/$username/.doil"
-#  chown -R $username:$username "/home/$username/.doil"
-#fi
+read -p "Please type your username for this machine: " username
+if [ ! -d "/home/$username/.doil" ]
+then
+  mkdir "/home/$username/.doil"
+  chown -R $username:$username "/home/$username/.doil"
+fi
