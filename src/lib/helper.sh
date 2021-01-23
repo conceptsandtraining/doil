0c14c298c9cdee82792422bfc355cda539c9f69f
Laura Herzog <quellmalz.laura@gmail.com>
2021-01-23 16:42:05 +0100

    Current state

--- src/lib/helper.sh
+++ src/lib/helper.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# doil is a tool that creates and manages multiple docker container
+# with ILIAS and comes with several tools to help manage everything.
+# It is able to download ILIAS and other ILIAS related software
+# like cate.
+#
+# Copyright (C) 2020 - 2021 Laura Herzog (laura.herzog@concepts-and-training.de)
+# Permission to copy and modify is granted under the AGPL license
+#
+# Contribute: https://github.com/conceptsandtraining/ilias-tool-doil
+#
+#                    .-.
+#                   / /
+#                  / |
+#    |\     ._ ,-""  `.
+#    | |,,_/  7        ;
+#  `;=     ,=(     ,  /
+#   |`q  q  ` |    \_,|
+#  .=; <> _ ; /  ,/'/ |
+# ';|\,j_ \;=\ ,/   `-'
+#     `--'_|\  )
+#    ,' | /  ;'
+#   (,,/ (,,/      Thanks to Concepts and Training for supporting doil
+#
+# Last revised 2021-mm-dd
+
 function doil_get_hash() {
   if [ -z "$1" ]
   then
