ecc7e21cd1e9caf7dd59c89303a5f59cf162c407
Laura Herzog <quellmalz.laura@gmail.com>
2021-12-10 11:57:08 +0100

    Hotfix

--- src/lib/pack/import/import.sh
+++ src/lib/pack/import/import.sh
@@ -190,7 +190,7 @@ fi
 docker exec -i ${INSTANCE}_${SUFFIX} bash -c '/etc/init.d/mariadb stop'
 docker exec -i ${INSTANCE}_${SUFFIX} bash -c 'chown -R mysql:mysql /var/lib/mysql'
 docker exec -i ${INSTANCE}_${SUFFIX} bash -c 'chown -R root:root /etc/mysql'
-docker exec -i ${INSTANCE}_${SUFFIX} bash -c '/etc/init.d/mariadb start'
+docker exec -i ${INSTANCE}_${SUFFIX} bash -c "service mysql restart"
 
 docker exec -i ${INSTANCE}_${SUFFIX} bash -c 'mysql -e "DROP DATABASE IF EXISTS ilias;"'
 docker exec -i ${INSTANCE}_${SUFFIX} bash -c 'mysql -e "CREATE DATABASE ilias;"'
