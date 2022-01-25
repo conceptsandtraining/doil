37932a840bf8ebb8d6ba02f725ed5c0dbd7b7875
Laura Herzog <quellmalz.laura@gmail.com>
2022-01-25 13:04:11 +0100

    Refactored some stuff due to unit testing

--- tests/tests/test-doil-help.sh
+++ tests/tests/test-doil-help.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-test_doil_display_instances_help() {
+test_doil_display_help() {
+  source ./src/lib/include/helper.sh
+  asset_string_contains "doil - ILIAS docker tool" "$(doil_display_help)"
+}
+
+test_doil_display_help_with_parameter() {
   source ./src/lib/include/helper.sh
   asset_string_contains "doil instances - manages the instances" "$(doil_display_help "instances")"
-}
-
-test_doil_display_repo_help() {
-  source ./src/lib/include/helper.sh
-  asset_string_contains "doil repo - manages the repositories" "$(doil_display_help "repo")"
-}
-
-test_doil_display_pack_help() {
-  source ./src/lib/include/helper.sh
-  asset_string_contains "doil pack - exports and imports a doil package" "$(doil_display_help "pack")"
-}
-
-test_doil_display_system_help() {
-  source ./src/lib/include/helper.sh
-  asset_string_contains "doil system - manages the doil system" "$(doil_display_help "system")"
 }
