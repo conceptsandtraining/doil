1f4b32e949fc2434395165a97c8b42618585dc48
Laura Herzog <quellmalz.laura@gmail.com>
2022-04-04 11:47:38 +0200

    Removed debug in update

--- src/updates/update-20220303.sh
+++ src/updates/update-20220303.sh
@@ -9,10 +9,10 @@ doil_update_20220303() {
   doil_system_add_group
   doil_system_create_folder
   doil_system_copy_doil
-  doil_system_setup_config
-  doil_system_setup_ip
-  doil_system_setup_access
-  doil_system_setup_userconfig
+#  doil_system_setup_config
+#  doil_system_setup_ip
+#  doil_system_setup_access
+#  doil_system_setup_userconfig
 
 	return 0
 }
