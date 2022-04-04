c182274bbdc17c2f50204f12df787783b0bd73e2
Laura <laura.herzog@concepts-and-training.de>
2022-04-04 12:00:52 +0200

    Update up.sh

--- src/lib/instances/up/up.sh
+++ src/lib/instances/up/up.sh
@@ -91,8 +91,8 @@ then
   # check proxy server
   /usr/local/bin/doil system:proxy start --quiet
 
-  # check maik server
-  doil system:mail start --quiet
+  # check mail server
+  /usr/local/bin/doil system:mail start --quiet
 
   # Start the container
   docker-compose up -d
