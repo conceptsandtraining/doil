969e788611fd03f7f5c99d4d14044278b42e1119
daniwe4 <daniel.weise@concepts-and-training.de>
2023-07-12 11:20:39 +0200

    dic states:341 extend cron state to handle ILIAS 9 cron jobs

--- app/src/Commands/Instances/CreateCommand.php
+++ app/src/Commands/Instances/CreateCommand.php
@@ -288,10 +288,15 @@ class CreateCommand extends Command
         }
         $this->writer->endBlock();
 
+        $ilias_version = $this->getIliasVersion($instance_path);
+
         // set grains
         $this->writer->beginBlock($output, "Setting up instance configuration");
         $mysql_password = $this->generatePassword(16);
-        $cron_password = $this->generatePassword(16);
+        $cron_password = "not-needed";
+        if ($ilias_version < 9) {
+            $cron_password = $this->generatePassword(16);
+        }
         $host = explode("=", $this->filesystem->getLineInFile("/etc/doil/doil.conf", "host"));
         $this->docker->setGrain($instance_salt_name, "mysql_password", $mysql_password);
         sleep(1);
@@ -337,7 +342,6 @@ class CreateCommand extends Command
 
         // apply composer state
         $this->writer->beginBlock($output, "Apply composer state");
-        $ilias_version = $this->getIliasVersion($instance_path);
         $this->docker->applyState($instance_salt_name, $this->getComposerVersion($ilias_version));
         $this->writer->endBlock();
 
