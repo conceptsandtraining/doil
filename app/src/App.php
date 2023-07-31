07d7ddd66ecd151894732ba86edfe4a6a25785da
daniwe4 <daniel.weise@concepts-and-training.de>
2023-07-31 10:32:51 +0200

    master cron: rm unecessary if statement

--- app/src/App.php
+++ app/src/App.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Command\Command;
 
 class App extends Application
 {
-    const NAME = "Doil Version 20230713 - build 2023-07-13";
+    const NAME = "Doil Version 20230731 - build 2023-07-31";
 
     public function __construct(Command ...$commands)
     {
