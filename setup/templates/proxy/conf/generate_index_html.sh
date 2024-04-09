ee374f2ee056a657db704546888af5cd977648e8
daniwe4 <daniel.weise@concepts-and-training.de>
2024-04-09 15:00:59 +0200

    master proxy: add an status page that list all active doil instances as links

---
+++ setup/templates/proxy/conf/generate_index_html.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+RESULT=""
+SERVER_NAME=$(sed -n -e '/server_name/p' /etc/nginx/conf.d/local.conf | grep -v '#' | head -n 1 | cut -d ' ' -f6 | cut -d ';' -f1)
+readarray -t INSTANCES < <(ls /etc/nginx/conf.d/sites)
+for i in "${INSTANCES[@]}"
+do
+        NAME=$(echo $i | cut -d . -f1)
+        RESULT=${RESULT}"<li><a href=\"http://${SERVER_NAME}/${NAME}/\" target=\"_blank\">${SERVER_NAME}/${NAME}</a></li>"
+done
+
+cat << EOF > /tmp/index.html
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Doil-Server-Status</title>
+    <style>
+      body { font-family: monospace; background: #002b36; color: #586e75; }
+      a { color: #2aa198; }
+      h2 { color: #268bd2; margin-bottom: -10px; }
+      p { color: #859900; margin-top: 0px; }
+      ul.index { margin-top: 0px; margin-bottom: 15px; }
+      li { color: #2aa198; }
+    </style>
+  </head>
+  <body>
+  <h2 id="instances">Doil Instances</h2>
+      <p>&nbsp;</p>
+      <ul class="index">
+      $(echo "${RESULT}")
+      </ul>
+  </body>
+  </html>
+EOF
+
+cp /tmp/index.html /var/www/html/index.html
