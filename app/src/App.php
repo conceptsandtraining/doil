e611c4034b2789f4e3f3705c8194ea8a0de1a663
daniwe4 <daniel.weise@concepts-and-training.de>
2024-09-25 15:53:26 +0200

    master php: add version 8.3

--- app/src/App.php
+++ app/src/App.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Command\Command;
 
 class App extends Application
 {
-    const NAME = "Doil Version 20240902 - build 2024-09-02";
+    const NAME = "Doil Version 20240926 - build 2024-09-26";
 
     public function __construct(Command ...$commands)
     {
