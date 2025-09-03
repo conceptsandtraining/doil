e8d102ac2f9afc243305ebf020145edb09a9baa8
daniwe4 <daniel.weise@concepts-and-training.de>
2025-09-03 11:06:36 +0200

    master imagemagick:519 use right path for source file

--- app/src/App.php
+++ app/src/App.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Command\Command;
 
 class App extends Application
 {
-    const NAME = "Doil Version 20250901 - build 2025-09-01";
+    const NAME = "Doil Version 20250903 - build 2025-09-03";
 
     public function __construct(Command ...$commands)
     {
