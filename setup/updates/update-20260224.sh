21a2e37db0374bc8bca7e0947688040887f44ac3
daniwe4 <daniel.weise@concepts-and-training.de>
2026-02-23 13:42:26 +0100

    master quick_fix: fix last release

--- setup/updates/update-20260224.sh
+++ setup/updates/update-20260224.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-doil_update_20260223() {
+doil_update_20260224() {
   cp -r ${SCRIPT_DIR}/../app/src/* /usr/local/lib/doil/app/src/
   cp -r ${SCRIPT_DIR}/../setup/stack/states/enable-saml/saml/SetIdpV10.php.j2  /usr/local/share/doil/stack/states/enable-saml/saml/SetIdpV10.php.j2
 
