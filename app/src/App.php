b4cd280e902ba34992ff534a4d030727f114f3eb
daniwe4 <daniel.weise@concepts-and-training.de>
2026-07-03 13:14:58 +0200

    master Update: add update file for 20260703

--- app/src/App.php
+++ app/src/App.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Command\Command;
 
 class App extends Application
 {
-    const NAME = "Doil Version 20260624 - build 2026-06-24";
+    const NAME = "Doil Version 20260707 - build 2026-07-07";
 
     public function __construct(Command ...$commands)
     {
