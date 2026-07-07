ff099d7af111481387b02798d08b4b860d3950d3
daniwe4 <daniel.weise@concepts-and-training.de>
2026-07-07 09:55:22 +0200

    master 567: add healthcheck to instances

--- setup/updates/update-20260707.sh
+++ setup/updates/update-20260707.sh
@@ -7,19 +7,25 @@ doil_update_20260707() {
   cp -r ${SCRIPT_DIR}/../setup/templates/mail/docker-compose.yml  /usr/local/lib/doil/server/mail/
   cp -r ${SCRIPT_DIR}/../setup/templates/proxy/docker-compose.yml  /usr/local/lib/doil/server/proxy/
 
-  apt-get install yq -y
+  apt-get install yq -y >/var/log/doil/stream.log 2>&1
 
   for DIR in /home/$SUDO_USER/.doil/instances/*
   do
-    yq -S '.services.[].healthcheck |= . + {"test": "curl -f http://localhost && mysql -u root --execute \"SHOW DATABASES;\" || exit 1", "interval": "10s", "timeout": "5s", "retries": "3", "start_period": "40s", "start_interval": "5s"}' "${DIR}"/docker-compose.yml > "${DIR}"/tmp.yml && mv "${DIR}"/tmp.yml "${DIR}"/docker-compose.yml
+    if [[ -d "$DIR" ]]
+    then
+      yq -S '.services.[].healthcheck |= . + {"test": "curl -f http://localhost && mysql -u root --execute \"SHOW DATABASES;\" || exit 1", "interval": "10s", "timeout": "5s", "retries": "3", "start_period": "40s", "start_interval": "5s"}' "${DIR}"/docker-compose.yml > "${DIR}"/tmp.yml && mv "${DIR}"/tmp.yml "${DIR}"/docker-compose.yml >/var/log/doil/stream.log 2>&1
+    fi
   done
 
   for DIR in $(doil_get_conf global_instances_path=)/*
   do
-    yq -S '.services.[].healthcheck |= . + {"test": "curl -f http://localhost && mysql -u root --execute \"SHOW DATABASES;\" || exit 1", "interval": "10s", "timeout": "5s", "retries": "3", "start_period": "40s", "start_interval": "5s"}' "${DIR}"/docker-compose.yml > "${DIR}"/tmp.yml && mv "${DIR}"/tmp.yml "${DIR}"/docker-compose.yml
+    if [[ -d "$DIR" ]]
+    then
+      yq -S '.services.[].healthcheck |= . + {"test": "curl -f http://localhost && mysql -u root --execute \"SHOW DATABASES;\" || exit 1", "interval": "10s", "timeout": "5s", "retries": "3", "start_period": "40s", "start_interval": "5s"}' "${DIR}"/docker-compose.yml > "${DIR}"/tmp.yml && mv "${DIR}"/tmp.yml "${DIR}"/docker-compose.yml >/var/log/doil/stream.log 2>&1
+    fi
   done
 
-  apt-get remove yq -y
+  apt-get remove yq -y >/var/log/doil/stream.log 2>&1
 
   return $?
 }
